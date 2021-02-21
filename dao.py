a69691d77d40e4c38440c650fa06a5a55261aad7
RebekahPerkins <r.perkins.smith@gmail.com>
2021-02-20 20:54:42 -0800

    encoding tuples with ast

--- dao.py
+++ dao.py
@@ -1,4 +1,5 @@
 import shelve
+import ast
 
 DIALOG_CHAIN_DB = 'dialog'
 NARRATION_CHAIN_DB = 'narration'
@@ -15,11 +16,11 @@ def load():
 def save_collection(d, db_name):
 	with shelve.open(db_name, 'c') as shelf:
 		for k, v in d.items():
-			shelf[k] = v
+			shelf[str(k)] = v
 
 def load_collection(db_name):
 	d = dict()
 	with shelve.open(db_name) as shelf:
 		for k, v in shelf.items():
-			d[k] = v
+			d[ast.literal_eval(k)] = v
 	return d
