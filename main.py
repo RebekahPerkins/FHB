fd2eed7428dfe999a1b64a4a26dc22802fbf82e5
RebekahPerkins <r.perkins.smith@gmail.com>
2021-02-22 20:20:47 -0800

    mvp

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 import argparse
+import time
+import os
 from dao import save, load
 from writer import random_text, flatten
 from reader import process_file
 
-def main(run_test=False, filename=None, n=500):
+def main(run_test=False, new_data=False, n=500):
 	suffix_maps = ''
-	if filename is not None:
-		suffix_maps = process_file(filename)
+	if new_data:
+		print("Refreshing Data.")
+		for db in ['narration.dat', 'narration.dir', 'narration.bak', 'dialog.dat', 'dialog.dir', 'dialog.bak']:
+			if os.path.exists(db):
+				os.remove(db)
+		for filename, encoding in ({'alp.txt': None, 'tsg.txt':'utf8', 'llf.txt':'utf8', 'hhc.txt':'utf8'}.items()):
+			suffix_maps = process_file(filename, encoding)
 		if not run_test:
 			save(suffix_maps)
+
+	time_0 = time.time()
+	print("Loading data...")
 	if not run_test:
 		suffix_maps = load()
+	time_1  = time.time()
+	print("Data loaded! Time:" + str(time_1 - time_0))
 	narration = random_text(suffix_maps[0], n)
+	time_2  = time.time()
+	print("Narration created! Time:" + str(time_2 - time_1))
 	dialog = random_text(suffix_maps[1], n)
+	time_3  = time.time()
+	print("Dialog created! Time:" + str(time_3 - time_2))
 	text = flatten(narration, dialog, n)
+	time_4  = time.time()
+	print("Text flattened! Time:" + str(time_4 - time_3))
+	print("##################################################################")
 	print(text)
-	if run_test:
-		test(text, n, filename)
 
-	#TODO fun options like genderqueer pronouns, combine characters
-	#TODO at least 2 bugs: space after dialog before quote. Occasionally 2 quotes together, not sure why
-
-def test(text, n, filename=None):
-	print("Checking test assertions...")
-	assert(len(text)) == n
-	#python .\main.py --run_test --filename='thelittletest2.txt' -n=34
-	if filename == 'thelittletest2.txt' and n == 34:
-		assert(text == 'Only one possibility. "This is it.')
-	print("Success!")
-
+# python .\main.py --new_data
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-R", "--run_test", help="test mode", action="store_true")
-	parser.add_argument("-f", "--filename", help="txt file to process")
+	parser.add_argument("-D", "--new_data", help="replace databases from files", action="store_true")
 	parser.add_argument("-n", "--num_chars", type=int, help="num of characters to write")
 	args = parser.parse_args()
-	kwargs={'run_test': args.run_test}
-	if args.filename:
-		kwargs['filename'] = args.filename
+	kwargs={'run_test': args.run_test, 'new_data': args.new_data}
 	if args.num_chars:
 		kwargs['n'] = args.num_chars
 	main(**kwargs)
