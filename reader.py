d5bd3f08449dcc4c47939f01160685792075f66e
RebekahPerkins <r.perkins.smith@gmail.com>
2021-02-22 20:22:18 -0800

    mvp

--- reader.py
+++ reader.py
@@ -1,35 +1,33 @@
 import random
+import nltk
+from character_merge import merge
 
 suffix_map = {}        # map from prefixes to a list of suffixes
 prefix = ()            # current tuple of words
 suffix_map_dialog = {}
 prefix_dialog = ()
 
-def process_file(filename, order=3):
-	fin = open(filename)
-	skip_header(fin, filename)
-
+def process_file(filename, encoding='cp1252', order=4):
+	fin = open(filename, encoding=encoding)
 	is_dialog = False
 	word_so_far = ''
 	line = fin.readline()
 	while line is not '':
 		line = skip_chapter_headers(fin, line)
-		if line.startswith("End of Project"):
-			break
-		for c in line:
-			if c in '".?!,':
-				if len(word_so_far) > 0:
-					process_word(word_so_far, is_dialog)
-					word_so_far = ''
-				process_word(c, is_dialog)
-			elif c.isspace():
-				if len(word_so_far) > 0:
-					process_word(word_so_far, is_dialog)
-					word_so_far = ''
-			else:
-				word_so_far += c
-			if c == '"':
-				is_dialog = not is_dialog
+		if encoding == 'utf8':
+			line = line.replace('“', '"').replace('”', '"').replace("’", "'").replace("‘", "'")
+		words = nltk.word_tokenize(line)
+		words_tagged = nltk.pos_tag(words)
+		for word, pos in words_tagged:
+			if pos == '``':
+				is_dialog = True
+				word = '"'
+			elif pos == "''":
+				is_dialog = False
+				word = '"'
+			elif pos == 'NNP':
+				word = merge(word)
+			process_word(word, is_dialog, order)
 		line = fin.readline()
 	fin.close()
 	return (suffix_map, suffix_map_dialog)
@@ -43,22 +41,7 @@ def skip_chapter_headers(fin, line):
 	else:
 		return line
 
-def skip_header(fin, filename):
-	start = None
-	if filename == '479-0.txt':
-		start = 'I'
-	elif filename == 'pg146.txt':
-		start = 'Sara'
-	elif filename == '113-0.txt':
-		start = ' THERE IS NO ONE LEFT'
-	if start == None:
-		return
-
-	for line in fin:
-		if line.startswith(start):
-			break
-
-def process_word(word, is_dialog, order=3):
+def process_word(word, is_dialog, order):
 	if is_dialog:
 		process_word_dialog(word, order)
 		return
@@ -76,7 +59,7 @@ def process_word(word, is_dialog, order=3):
 
 	prefix = shift(prefix, word)
 
-def process_word_dialog(word, order=3):
+def process_word_dialog(word, order):
 	global prefix_dialog
 
 	if len(prefix_dialog) < order:
